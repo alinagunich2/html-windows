1cc9b5b1ff57c2429220f6c255fba58021827e33
alinagunich2 <gunich.alina@gmail.com>
2024-04-12 11:05:55 +0300

    show js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const buttonsHide = document.querySelectorAll(".button-hide");
 const buttonsShow = document.querySelectorAll(".button-show");
+const buttonsSvg = document.querySelectorAll(".button-svg");
 const contentHide = document.querySelectorAll(".content-services__hide");
 
 buttonsHide.forEach((button, index) => {
   button.addEventListener("click", () => {
-    contentHide[index].style.display = "none";
+    contentHide[index].classList.remove("open");
+    buttonsSvg[index].classList.remove("open");
     button.style.display = "none";
     buttonsShow[index].style.display = "block";
   });
@@ -12,8 +14,20 @@ buttonsHide.forEach((button, index) => {
 
 buttonsShow.forEach((button, index) => {
   button.addEventListener("click", () => {
-    contentHide[index].style.display = "block";
+    contentHide[index].classList.add("open");
+    buttonsSvg[index].classList.add("open");
     button.style.display = "none";
     buttonsHide[index].style.display = "block";
   });
 });
+
+const text = document.getElementById("text");
+const openBtn = document.getElementById("open");
+const hideBtn = document.getElementById("hide");
+
+openBtn.onclick = function () {
+  text.classList.add("open");
+};
+hideBtn.onclick = function () {
+  text.classList.remove("open");
+};
