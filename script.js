df070ce161d807a740bed58e8e2db6c4a389bbb4
alinagunich2 <gunich.alina@gmail.com>
2024-04-15 15:48:05 +0300

    added scroll

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const buttonsHide = document.querySelectorAll(".button-hide");
 const buttonsShow = document.querySelectorAll(".button-show");
 const buttonsSvg = document.querySelectorAll(".button-svg");
 const contentHide = document.querySelectorAll(".content-services__hide");
+const menuLink = Array.from(document.getElementsByClassName("menu__link"));
 
 buttonsHide.forEach((button, index) => {
   button.addEventListener("click", () => {
@@ -36,3 +37,39 @@ cross.addEventListener("click", function () {
   cross.style.display = "none";
   burger.style.display = "block";
 });
+
+menuLink.forEach((link)=>{
+  link.addEventListener("click", () => {
+    menu.classList.remove("open");
+    cross.style.display = "none";
+    burger.style.display = "block";
+  });
+})
+
+let headerPadding = Array.from(document.getElementsByClassName('header-padding'))
+menuLink.forEach((link,index)=>{
+    link.addEventListener("click", function () {
+      headerPadding.forEach((padTop)=>{
+        padTop.style.paddingTop = "0px"
+      })
+      if(window.innerWidth >= 1280){
+        if(index === 1){
+          headerPadding[index].style.paddingTop = "50px"
+        }
+      }else if( window.innerWidth <= 1280 && window.innerWidth >= 800){
+          headerPadding[index].style.paddingTop = "50px"
+      }else if(window.innerWidth <= 800 && window.innerWidth >= 424){
+        headerPadding[index].style.paddingTop = "90px"
+      }else if(window.innerWidth <= 424 && window.innerWidth >= 385){
+        headerPadding[index].style.paddingTop = "60px"
+      }else if(window.innerWidth <= 386 ){
+        if(index !== 1 && index !== 0){
+          headerPadding[index].style.paddingTop = "100px"
+        }else{
+          headerPadding[index].style.paddingTop = "130px"
+        }
+      }
+     });
+  // }
+  })
+
